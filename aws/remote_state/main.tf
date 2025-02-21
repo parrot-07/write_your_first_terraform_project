3a74dade070043e255ce07609515031b7b02f695
gayathri <gayathridevidasoju@gmail.com>
2025-02-22 04:35:23 +0530

    Update main.tf remote state
    Update main.tf remote state

--- aws/remote_state/main.tf
+++ aws/remote_state/main.tf
@@ -9,8 +9,20 @@
 # ----------------------------------------------------------------------------------------------------------------------
 
 terraform {
+    backend "s3" {
+    encrypt = true
+  }
   required_version = ">= 0.12"
 }
+# example of 'partial configuration':
+# https://www.terraform.io/docs/backends/config.html#partial-configuration
+#
+# cat config/backend-dev.conf
+bucket  = "<account_id>-terraform-states"
+key     = "development/service-name.tfstate"
+encrypt = true
+region  = "ap-southeast-2"
+dynamodb_table = "terraform-lock"
 
 # ------------------------------------------------------------------------------
 # CONFIGURE OUR AWS CONNECTION
